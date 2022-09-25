4d61c77c7699a20b76a5fd18e4fadf708f8d6213
Isaack Mungui <munguiisaack@gmail.com>
2022-09-25 12:25:56 +0300

    Fetched data from local endpoint and rendered to DOM

--- code-challenge/src/components/Transaction.js
+++ code-challenge/src/components/Transaction.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Transaction() {
+  const [transactions, setTransactions] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:8001/transactions')
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      setTransactions(data)
+    })
+  }, [])
+
   return (
     <tr>
-      <td>{"your code here..."}</td>
-      <td>{"your code here..."}</td>
-      <td>{"your code here..."}</td>
-      <td>{"your code here..."}</td>
+      <td>{transactions.map(transaction => transaction.date)}</td>
+      <td>{transactions.map(transaction => transaction.description)}</td>
+      <td>{transactions.map(transaction => transaction.category)}</td>
+      <td>{transactions.map(transaction => transaction.amount)}</td> 
     </tr>
   );
 }
