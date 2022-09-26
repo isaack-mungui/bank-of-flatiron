a340f1a138011942e7c1f499c848f420f8bfa62f
Isaack Mungui <munguiisaack@gmail.com>
2022-09-26 13:19:35 +0300

    Fetched data and passed to transactionlist component

--- code-challenge/src/components/AccountContainer.js
+++ code-challenge/src/components/AccountContainer.js
@@ -1,14 +1,35 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
+
 function AccountContainer() {
+  // const [search, setSearch] = useState("")
+  const [transactions, setTransactions] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:8001/transactions')
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      setTransactions(data)
+    })
+  }, [])
+
+  // function handleSearch(search) {
+  //   let searchTerm = transactions.description.toLowerCase().includes(search)
+  //   setTransactions(transactions.filter((transaction) => transaction.description === searchTerm))
+  //   // return setTransactions(search.toLowerCase() === '' ? setTransactions(transactions) : setTransactions(searchTerm))
+  // }
+
+
   return (
     <div>
-      <Search />
+      {/* <Search search={handleSearch} /> */}
       <AddTransactionForm />
-      <TransactionsList />
+      <TransactionsList transaction={transactions} />
     </div>
   );
 }
