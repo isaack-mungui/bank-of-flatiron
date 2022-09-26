e25be4a270b56fe1355c4b8ca6f4a0e706563634
Isaack Mungui <munguiisaack@gmail.com>
2022-09-26 13:22:01 +0300

    Passed in props to component

--- code-challenge/src/components/Transaction.js
+++ code-challenge/src/components/Transaction.js
@@ -1,28 +1,11 @@
-import React, { useEffect, useState } from "react";
-
-function Transaction({id}) {
-  const [transactions, setTransactions] = useState([])
-
-  useEffect(() => {
-    fetch('http://localhost:8001/transactions')
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      setTransactions(data)
-    })
-  }, [])
-
+function Transaction({date, description, category, amount}) {
   return (
-    <div>
-      {transactions.map((transaction) => (
-        <tr>
-          <td key={id}>{transaction.date}</td>
-          <td key={id}>{transaction.description}</td>
-          <td key={id}>{transaction.category}</td>
-          <td key={id}>{transaction.amount}</td> 
-        </tr>
-      ))}
-    </div>
+    <tr key>
+      <td>{date}</td>
+      <td>{description}</td>
+      <td>{category}</td>
+      <td>{amount}</td> 
+    </tr>
   );
 }
 
